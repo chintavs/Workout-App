58d7b6bc53b0f38cb0382fdd8239dbf3d52e8480
daltonco <daltonco@mail.uc.edu>
2023-02-16 16:59:23 -0500

    Created GroupTests.kt and added testImplementation dependency

---
+++ app/src/test/java/com/example/workoutapp/GroupTests.kt
@@ -0,0 +1,10 @@
+package com.example.workoutapp
+
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+class GroupTests {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+}
