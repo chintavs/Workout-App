78e84dac4e4d68ab0b6d16af9c0ad99a3ca2c0ae
kooncewc <kooncewc@mail.uc.edu>
2023-02-22 20:26:46 -0500

    Basic Home Screen and Profile Page

---
+++ com.workoutapp.ui/ui.theme/Color.kts
@@ -0,0 +1,9 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Grey100 = Color(0xFFF2F2F2)
+val Orange200 = Color(0xFFD97762)
+val Orange = Color(0xFFF26E50)
+val Teal200 = Color(0xFF5BCCD9)
+val Teal300 = Color(0xFF2E6ea6)
