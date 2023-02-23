9b929e01626a8d3ca898532aae6f3a8eef12c37a
kooncewc <kooncewc@mail.uc.edu>
2023-02-22 21:15:42 -0500

    -Added Profile Page -Updated Theme folder

--- app/src/main/java/com/example/workoutapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/workoutapp/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.workoutapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Grey100 = Color(0xFFF2F2F2)
+val Orange200 = Color(0xFFD97762)
+val Orange = Color(0xFFF26E50)
+val Teal200 = Color(0xFF5BCCD9)
+val Teal300 = Color(0xFF2E6ea6)
