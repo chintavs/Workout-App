bdeb5c75a375345fbf7f5518b01de76b20c9672d
belmonjd1 <belmonjd@mail.uc.edu>
2023-03-03 17:50:08 -0500

    Some mild refactoring; removed some unnecessary code.

--- app/src/test/java/com/example/workoutapp/WorkoutUnitTest.kt
+++ app/src/test/java/com/example/workoutapp/WorkoutUnitTest.kt
@@ -47,7 +47,6 @@ class WorkoutUnitTest {
         Dispatchers.setMain(mainThreadSurrogate)
         MockKAnnotations.init(this)
         gam = GroupsActivity()
-
     }
 
     @After
@@ -92,15 +91,12 @@ class WorkoutUnitTest {
     //The json service we are using has split json files we need to parse so this will likely change as
     //we research how to make it work.
     @Test
-    fun `Given service connects to Countries JSON stream when data are read and parsed then country collection should be greater than zero`() =runTest {
+    fun `Given service connects to Workouts JSON stream when data are read and parsed then workout collection should be greater than zero`() =runTest {
             launch(Dispatchers.Main) {
                 givenWorkoutServiceIsInitialized()
                 whenServiceDataAreReadAndParsed()
                 thenTheWorkoutCollectionSizeShouldBeGreaterThanZero()
-
             }
-
-
         }
 
     private fun givenWorkoutServiceIsInitialized() {
@@ -117,20 +113,20 @@ class WorkoutUnitTest {
     }
 
     private fun givenViewModelIsInitializedWithMockData() {
-        val countries = ArrayList<Workout>()
-        countries.add(Workout("3/4 Sit-Up", "pull", "beginner", "compound", "body only", "abdominals", "[]",
+        val workouts = ArrayList<Workout>()
+        workouts.add(Workout("3/4 Sit-Up", "pull", "beginner", "compound", "body only", "abdominals", "[]",
             "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
             "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
             "Flex your hips and spine to raise your torso toward your knees.",
             "At the top of the contraction your torso should be perpendicular to the ground. Reverse the motion, going only Â¾ of the way down.",
             "Repeat for the recommended amount of repetitions."))
-        countries.add(Workout("Barbell Curl", "pull", "beginner", "isolation", "barbell", "biceps", "forearms",
+        workouts.add(Workout("Barbell Curl", "pull", "beginner", "isolation", "barbell", "biceps", "forearms",
             "Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. This will be your starting position.",
             "While holding the upper arms stationary, curl the weights forward while contracting the biceps as you breathe out. Tip: Only the forearms should move.",
             "Continue the movement until your biceps are fully contracted and the bar is at shoulder level. Hold the contracted position for a second and squeeze the biceps hard.",
             "Slowly begin to bring the bar back to starting position as your breathe in.",
             "Repeat for the recommended amount of repetitions.", "strength"))
-        countries.add(Workout("Ab Crunch Machine", "pull", "intermediate", "isolation", "machine",
+        workouts.add(Workout("Ab Crunch Machine", "pull", "intermediate", "isolation", "machine",
             "[abdominals]", " ",
             "Select a light resistance and sit down on the ab machine placing your feet under the pads provided and grabbing the top handles. Your arms should be bent at a 90 degree angle as you rest the triceps on the pads provided. This will be your starting position.",
             "At the same time, begin to lift the legs up as you crunch your upper torso. Breathe out as you perform this movement. Tip: Be sure to use a slow and controlled motion. Concentrate on using your abs to move the weight while relaxing your legs and feet.",
@@ -140,18 +136,17 @@ class WorkoutUnitTest {
 
         coEvery {workoutService.fetchWorkouts()} returns workouts
 
-        gam.workoutService = mockworkoutService
+        gam.workoutService = mockWorkoutService
     }
 
     private fun whenJSONDataAreReadAndParsed() {
         gam.fetchWorkouts()
     }
     private fun thenResultsShouldContainBelize() {
-        var allCountries : List<Workout>? = ArrayList<Workout>()
         val latch = CountDownLatch(1);
         val observer = object : Observer<List<Workout>> {
             override fun onChanged(t: List<Workout>?) {
-                allCountries = t
+                allWorkouts = t
                 latch.countDown()
                 gam.workouts.removeObserver(this)
             }
@@ -159,8 +154,8 @@ class WorkoutUnitTest {
         gam.workouts.observeForever(observer)
 
         latch.await(1, TimeUnit.SECONDS)
-        assertNotNull(allCountries)
-        assertTrue(allCountries!!.contains(Workout("3/4 Sit-Up", "pull", "beginner", "compound", "body only", "abdominals", "[]",
+        assertNotNull(allWorkouts
+        assertTrue(allWorkouts!!.contains(Workout("3/4 Sit-Up", "pull", "beginner", "compound", "body only", "abdominals", "[]",
             "Lie down on the floor and secure your feet. Your legs should be bent at the knees.",
             "Place your hands behind or to the side of your head. You will begin with your back on the ground. This will be your starting position.",
             "Flex your hips and spine to raise your torso toward your knees.",
