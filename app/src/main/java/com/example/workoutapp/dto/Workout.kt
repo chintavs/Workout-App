82823694de0d5e8c900036d722d2bc05ea22b1d6
spauldvl <spauldvl@mail.uc.edu>
2023-03-04 21:50:59 -0500

    Code review 1 by vyviene spaulding

---
+++ app/src/main/java/com/example/workoutapp/dto/Workout.kt
@@ -0,0 +1,5 @@
+package com.example.workoutapp.dto
+
+class Workout {
+
+}
--- app/src/main/java/com/example/workoutapp/service/WorkoutService.kt
+++ app/src/main/java/com/example/workoutapp/service/WorkoutService.kt
@@ -1,6 +1,24 @@
 package com.example.workoutapp.service
 
-class WorkoutService {
-    fun fetchWorkouts(){
+import com.example.workoutapp.RetrofitClientInstance
+import com.example.workoutapp.dao.IWorkoutDAO
+import com.example.workoutapp.dto.Workout
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import retrofit2.awaitResponse
+
+interface IWorkoutService {
+    suspend fun fetchWorkouts() : List<Workout>?
+}
+class WorkoutService : IWorkoutService {
+
+    override suspend fun fetchWorkouts() : List<Workout>? {
+        return withContext(Dispatchers.IO) {
+            val service = RetrofitClientInstance.retrofitInstance?.create(IWorkoutDAO::class.java)
+            val workouts = async {service?.getAllWorkouts()}
+            var result = workouts.await()?.awaitResponse()?.body()
+            return@withContext result
+        }
     }
 }
