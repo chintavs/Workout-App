d72c9f0c0144b498d4680215d825c7331664e6bb
spauldvl <spauldvl@mail.uc.edu>
2023-03-04 20:32:48 -0500

    Code review 1 by vyviene spaulding

--- app/src/main/java/com/example/workoutapp/service/WorkoutService.java
+++
@@ -1,4 +0,0 @@
-package com.example.workoutapp.service;
-
-public class WorkoutService {
-}
--- app/src/test/java/com/example/workoutapp/WorkoutUnitTest.kt
+++ app/src/test/java/com/example/workoutapp/WorkoutUnitTest.kt
@@ -1,18 +1,14 @@
 package com.example.workoutapp
 
 import androidx.lifecycle.Observer
-import com.example.workoutapp.service.WorkoutService
 import org.junit.Test
 
 import org.junit.Assert.*
 
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
@@ -21,8 +17,6 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 
 import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TestRule
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
