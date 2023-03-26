238f7ab4f2a5bfec4ad6853a2e4c4b15cf3a3905
VishvakCh <95390151+chintavs@users.noreply.github.com>
2023-03-26 17:37:09 -0400

    Polished the record workout form

--- app/src/main/java/com/example/workoutapp/RecordActivity.kt
+++ app/src/main/java/com/example/workoutapp/RecordActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.workoutapp.ui.theme.WorkoutAppTheme
 
 class RecordActivity : ComponentActivity() {
@@ -42,29 +44,58 @@ fun RecordWO() {
     var Reps by remember { mutableStateOf("") }
     var Time by remember { mutableStateOf("") }
     val context = LocalContext.current
-    Column {
+    Column (
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Record Workout",
+            style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
         OutlinedTextField(
             value = Name,
+            modifier = Modifier
+                .padding(10.dp),
             onValueChange = {Name =  it},
             label = {Text (stringResource(R.string.workoutName))})
         OutlinedTextField(
             value = Reps,
+            modifier = Modifier
+                .padding(10.dp),
             onValueChange = {Reps =  it},
             label = {Text (stringResource(R.string.Reps))})
         OutlinedTextField(
             value = Sets,
+            modifier = Modifier
+                .padding(10.dp),
             onValueChange = {Sets =  it},
             label = {Text (stringResource(R.string.Sets))})
         OutlinedTextField(
             value = Time,
+            modifier = Modifier
+                .padding(10.dp),
             onValueChange = {Time =  it},
             label = {Text (stringResource(R.string.Time))})
+
         Button(
             onClick = {
                 Toast.makeText(context, "$Name $Sets $Reps $Time", Toast.LENGTH_LONG).show()
             })
         {
             Text(text = "Save")
+        }
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
+            })
+        {
+            Text(text = "Cancel")
         }
     }
 }
