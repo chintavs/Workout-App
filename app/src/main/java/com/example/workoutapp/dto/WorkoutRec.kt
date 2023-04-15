0c6e8789f7562863ccaf1a2f9917cb27d5e92e0b
arthursr01 <70337560+arthursr01@users.noreply.github.com>
2023-04-15 18:27:54 -0400

    Update WorkoutRec.kt

--- app/src/main/java/com/example/workoutapp/dto/WorkoutRec.kt
+++ app/src/main/java/com/example/workoutapp/dto/WorkoutRec.kt
@@ -18,6 +18,39 @@ data class WorkoutRec(@SerializedName("name") var workoutName: String, val force
         return "$workoutName"
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as WorkoutRec
+
+        if (workoutName != other.workoutName) return false
+        if (force != other.force) return false
+        if (level != other.level) return false
+        if (mechanic != other.mechanic) return false
+        if (equipment != other.equipment) return false
+        if (!primaryMuscles.contentEquals(other.primaryMuscles)) return false
+        if (!secondaryMuscles.contentEquals(other.secondaryMuscles)) return false
+        if (!instructions.contentEquals(other.instructions)) return false
+        if (category != other.category) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = workoutName.hashCode()
+        result = 31 * result + force.hashCode()
+        result = 31 * result + level.hashCode()
+        result = 31 * result + mechanic.hashCode()
+        result = 31 * result + equipment.hashCode()
+        result = 31 * result + primaryMuscles.contentHashCode()
+        result = 31 * result + secondaryMuscles.contentHashCode()
+        result = 31 * result + instructions.contentHashCode()
+        result = 31 * result + category.hashCode()
+        result = 31 * result + id
+        return result
+    }
 
 
 }
